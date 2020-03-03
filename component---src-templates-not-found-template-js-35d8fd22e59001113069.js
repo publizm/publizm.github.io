6ed54f7940328f98c53e7101a384497f002f483f
publizm <publizm@gmail.com>
2020-03-03 21:40:28 +0900

    Updates

---
+++ component---src-templates-not-found-template-js-35d8fd22e59001113069.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{256:function(t,e,n){"use strict";n.r(e);var a=n(0),i=n.n(a),l=n(251),s=n(250),u=n(252),o=n(74);e.default=function(){var t=Object(o.b)(),e=t.title,n=t.subtitle;return i.a.createElement(s.a,{title:"Not Found - "+e,description:n},i.a.createElement(l.a,null),i.a.createElement(u.a,{title:"NOT FOUND"},i.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-templates-not-found-template-js-35d8fd22e59001113069.js.map
--- content/pages/contacts.md
+++
@@ -1,9 +0,0 @@
----
-title: "Contact me"
-template: "page"
-socialImage: "/media/image-4.jpg"
----
-
-publizm@gmail.com
-
-![Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.](/media/image-4.jpg)
